61d18e4f34238e8d12f5bb7a52398ffacc9de547
杨凸伐 <yangtufa@yangtufadeMacBook-Pro.local>
2017-10-12 09:25:33 +0800

    first

---
+++ app/src/main/java/web/tuhua/com/websocketapp/MainActivity.java
@@ -0,0 +1,15 @@
+package web.tuhua.com.websocketapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WsManager.getInstance().init();
+    }
+}
---
+++ app/src/main/java/web/tuhua/com/websocketapp/WsStatus.java
@@ -0,0 +1,11 @@
+package web.tuhua.com.websocketapp;
+
+/**
+ * Created by yangtufa on 2017/10/12.
+ */
+
+public enum  WsStatus {
+    CONNECT_SUCCESS,//连接成功
+    CONNECT_FAIL,//连接失败
+    CONNECTING;//正在连接
+}
